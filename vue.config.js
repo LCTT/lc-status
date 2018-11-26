3affdc7f3d10da2d1608d34b94f7c8c5b898de26
Wenxuan Zhao <viz@linux.com>
2018-11-26 12:00:10 -0800

    Create 404 symlink for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+var SymlinkWebpackPlugin = require('symlink-webpack-plugin')
+
 module.exports = {
+  configureWebpack: (config) => {
+    config.plugins.push(new SymlinkWebpackPlugin({
+      origin: 'index.html',
+      symlink: '404.html'
+    }))
+  },
   transpileDependencies: ['vuex-module-decorators']
 }
